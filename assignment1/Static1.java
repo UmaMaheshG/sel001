bd5ad0f15ecc1aa96c1d8e4d92f2d459b9097e48
ramanadevi <ramanadevi.p@gmail.con>
2015-10-13 23:00:21 +0530

    calling static and non static methods in Static1 class from StaticnonStatic class

---
+++ assignment1/Static1.java
@@ -0,0 +1,24 @@
+/**
+ * Created by PRANAV on 10/13/2015.
+ */
+public class Static1 {
+
+    public int add(int a,int b)
+    {
+
+        int c=a+b;
+        System.out.println("addition from nonstatic method is" + c);
+        return c;
+    }
+
+    public static int mul(int a,int b)
+    {
+
+        int c=a*b;
+        System.out.println("multiplication from static method is" + c);
+        return c;
+    }
+
+
+
+}
---
+++ assignment1/StaticnonStatic.java
@@ -0,0 +1,30 @@
+/**
+ * Created by PRANAV on 10/13/2015.
+ */
+public class StaticnonStatic {
+
+    public void nonstaticmethod()
+    {
+        System.out.println("message from nonstatic method" );
+    }
+
+    public static void staticmethod()
+    {
+        System.out.println("message from static method ");
+    }
+
+public static void main(String []args)
+{
+
+    StaticnonStatic stn=new StaticnonStatic();
+    stn.nonstaticmethod();
+    staticmethod();
+    Static1 st=new Static1();//different class object canbe created in this class
+    st.add(1,2);    //a non static method should be called using object
+    //Static1.add(2,3);This is incorrect as it is a nonstatic method it needs object
+    Static1.mul(2,3); //a static method can be called using class name
+
+
+}
+
+}
