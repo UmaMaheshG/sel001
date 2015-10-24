e79c39d4509b17cb8c5c8a5a75e94fd92dae9d76
ramanadevi <ramanadevi.p@gmail.con>
2015-10-24 18:39:36 +0530

    using super keyword

---
+++ assignment1/Superkeywordparent.java
@@ -0,0 +1,14 @@
+/**
+ * Created by PRANAV on 10/24/2015.
+ */
+public class Superkeywordparent
+{    int super1=50;
+    public void add()
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+      System.out.println("sum from super class is   "+ c)  ;
+    }
+
+}
