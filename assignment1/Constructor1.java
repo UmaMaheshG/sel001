86dafb6d378abe62cbe4489c82319177fbec41f2
ramanadevi <ramanadevi.p@gmail.con>
2015-10-25 13:28:39 +0530

    constructor

---
+++ assignment1/Constructor1.java
@@ -0,0 +1,18 @@
+/**
+ * Created by PRANAV on 10/25/2015.
+ */
+public class Constructor1
+
+{
+ public Constructor1(int b)
+ {
+
+     System.out.println("number is  "+b);
+ }
+    public static void main(String []args)
+    {
+
+        Constructor1 c=new Constructor1(8);
+    }
+
+}
