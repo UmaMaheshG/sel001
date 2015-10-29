4a781b039fd9cf91d97909d11bb248eda5068420
kambam saraswathi <alisettysashidhar@gmail.com>
2015-10-29 09:24:36 +0530

    New java code

---
+++ Strln.java
@@ -0,0 +1,9 @@
+public class Strln
+{
+	public static void main(String[] args)
+		{
+			String n = "Selenium Training";
+				System.out.print("String Length :" );
+				System.out.println(n.length());
+		}
+}
