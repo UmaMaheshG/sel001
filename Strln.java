0ca082980f4fe9cf2657853ab494a72d22de820b
Uma Mahesh G <gunipati81@users.noreply.github.com>
2015-10-29 10:31:14 +0530

    Delete Strln.java

--- Strln.java
+++
@@ -1,9 +0,0 @@
-public class Strln
-{
-	public static void main(String[] args)
-		{
-			String n = "Selenium Training";
-				System.out.print("String Length :" );
-				System.out.println(n.length());
-		}
-}
